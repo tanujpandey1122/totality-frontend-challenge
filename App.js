ce959309ce48ac87fb4674f7b6f18bdecfc4e331
tanujpandey1122 <139033624+tanujpandey1122@users.noreply.github.com>
2024-08-16 15:13:25 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,31 +1,24 @@
-import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import PropertyList from './components/PropertyList';
-import Cart from './components/Cart';
-import Checkout from './components/Checkout';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  const [cart, setCart] = useState([]);
-
-  const addToCart = (property) => {
-    setCart([...cart, property]);
-  };
-
-  const updateCart = (updatedCart) => {
-    setCart(updatedCart);
-  };
-
   return (
-    <Router>
-      <div>
-        <h1>Welcome to the Property Rental Platform!</h1>
-        <Routes>
-          <Route path="/" element={<PropertyList addToCart={addToCart} />} />
-          <Route path="/cart" element={<Cart cart={cart} updateCart={updateCart} />} />
-          <Route path="/checkout" element={<Checkout cart={cart} />} />
-        </Routes>
-      </div>
-    </Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
